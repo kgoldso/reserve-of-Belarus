bd56ca985c4292384cd0925bd026568e26bf12b8
kgoldso <123180174+kgoldso@users.noreply.github.com>
2024-02-05 22:13:55 +0300

    Add files via upload

---
+++ html/js/main.js
@@ -0,0 +1,40 @@
+function redirectToPage(pageURL) {
+    window.location.href = pageURL;
+}
+
+const imgBox = document.querySelector('.imgBox');
+const slides = imgBox.getElementsByTagName('img');
+var i = 0;
+
+function prevSlide() {
+    slides[i].classList.remove('active');
+    i = (i - 1 + slides.length) % slides.length;
+    slides[i].classList.add('active');
+}
+
+function nextSlide() {
+    slides[i].classList.remove('active');
+    i = (i + 1) % slides.length;
+    slides[i].classList.add('active');
+}
+
+const contentBox = document.querySelector('.contentBox');
+const slidesText = contentBox.getElementsByTagName('div');
+var j = 0;
+
+function prevSlideText() {
+    slidesText[j].classList.remove('active');
+    j = (j - 1 + slidesText.length) % slidesText.length;
+    slidesText[j].classList.add('active');
+}
+
+function nextSlideText() {
+    slidesText[j].classList.remove('active');
+    j = (j + 1) % slidesText.length;
+    slidesText[j].classList.add('active');
+}
+
+window.onload = function() {
+    // Прокрутить вверх страницы при загрузке
+    window.scrollTo(0, 0);
+}
